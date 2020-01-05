080790f87823d194694bdb2443c8dc26ceac86d9
Amanda Rolon <abrolon87@gmail.com>
2020-01-05 16:28:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,15 @@ class Song
   def initialize(name)
     @name = name
   end
-  
+
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+
   def self.all
     @@all
   end
