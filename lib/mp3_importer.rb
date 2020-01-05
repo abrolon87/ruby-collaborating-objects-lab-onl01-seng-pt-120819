dc3dce1661934163c91da0733a6ce2d719989b56
Amanda Rolon <abrolon87@gmail.com>
2020-01-05 17:02:35 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,11 +1,16 @@
 class MP3Importer
-  attr_accessor :path 
+  attr_accessor :path
+
   def initialize(path)
     @path = path
   end
 
+  def files
+    @files
+  end
+
   def import(list_of_filenames)
-    file.each{ |filename| Song.new_by_filename(filename) }
+    files.each{ |filename| Song.new_by_filename(filename) }
   end
 
 end
