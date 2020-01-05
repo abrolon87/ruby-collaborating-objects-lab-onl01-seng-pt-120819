17a6a5787c76d0e6d1501488c6101b2112476375
Amanda Rolon <abrolon87@gmail.com>
2020-01-05 16:35:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,12 @@ class Song
 
   def initialize(name)
     @name = name
-    @@all << self 
+    @@all << self
+  end
+  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
   end
 
   def artist_name=(name)
