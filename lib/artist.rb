0b30163fb1390b8696da0e6d4ee5f7eee2d8d99e
Amanda Rolon <abrolon87@gmail.com>
2020-01-05 16:43:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,7 +11,7 @@ class Artist
     song.artist = self
   end
 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.new(name)
   end
 
