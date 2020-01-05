e63e533f918c223e9a87768e925a8ffd97256931
Amanda Rolon <abrolon87@gmail.com>
2020-01-05 16:37:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,10 @@ class Song
   attr_accessor :name, :artist
   @@all = []
 
-  # def initialize(name)
-  #   @name = name
-  #   @@all << self
-  # end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
   def self.new_by_filename(filename)
     song = self.new
     song.title = filename.split(" - ")[1]
