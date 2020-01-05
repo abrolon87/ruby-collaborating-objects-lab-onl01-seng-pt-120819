6fd6fc8655a6a376a25c9b625f7026b20c2dd33a
Amanda Rolon <abrolon87@gmail.com>
2020-01-05 16:57:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,9 +18,13 @@ class Artist
   def self.find(name)
     self.all.find {|artist| artist.name == name}
   end
-  
+
   def songs
     Song.all.select {|song| song.artist == self}
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
   end
 
   def save
